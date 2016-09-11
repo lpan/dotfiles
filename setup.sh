c2f5ca652cc1cfc5bed3d134086b326f5d0b34d9
Lawrence Pan <lawrencefeipan@gmail.com>
2016-09-11 14:41:30 -0400

    add antigen setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 INSTALL_VIM_PLUG='curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+INSTALL_ANTIGEN='git clone https://github.com/zsh-users/antigen ~/.antigen/antigen'
+
 INSTALL_OMZ='sh -c "$(curl -fsSL
 https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
 
@@ -24,6 +26,8 @@ confirmation() {
 confirmation "Install vim plug?" "$INSTALL_VIM_PLUG"
 
 confirmation "Install OH MY ZSH?" "$INSTALL_OMZ"
+
+confirmation "Install Antigen?" "$INSTALL_ANTIGEN"
 
 confirmation "Install Tmux Plugin Manager?" "$INSTALL_TPM"
 
